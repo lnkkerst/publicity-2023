5bae0fe824f82dc21d4c7d5be5843a5ad690d691
lnkkerst <lnkkerst04@gmail.com>
2023-08-25 16:34:04 +0800

    feat(live2d): use cdn, add loading progress

--- components/Live2d.vue
+++ components/Live2d.vue
@@ -10,6 +10,7 @@ const props = withDefaults(
 
 const container = ref<HTMLDivElement>();
 const canvas = ref<HTMLCanvasElement>();
+const loading = ref(true);
 
 onMounted(async () => {
   if (!canvas.value) {
@@ -22,17 +23,21 @@ onMounted(async () => {
   import('pixi-live2d-display').then(async live2d => {
     live2d.Live2DModel.registerTicker(Ticker);
     const model = await live2d.Live2DModel.from(
-      '/live2d_models/Pio/index.json'
+      'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/model/Potion-Maker/Pio/index.json'
     );
     model.scale = { x: props.scale, y: props.scale };
     app.renderer.resize(model.width, model.height);
     app.stage.addChild(model);
+    loading.value = false;
   });
 });
 </script>
 
 <template>
-  <div ref="container" class="w-fit h-fit z-100">
+  <div ref="container" class="w-fit h-fit z-100 relative">
+    <VOverlay contained :model-value="loading" class="grid place-items-center">
+      <VProgressCircular indeterminate size="large"></VProgressCircular>
+    </VOverlay>
     <canvas ref="canvas"></canvas>
   </div>
 </template>
