f3f386a00c15282502826073365b3897c2e11fdd
lnkkerst <lnkkerst04@gmail.com>
2023-09-04 21:25:49 +0800

    feat: add zx qrcode

--- components/QqGroupAside.vue
+++ components/QqGroupAside.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { variants } from '@catppuccin/palette';
-import QqGroupQrImg from '~/assets/img/qqgroupqr.png';
+import QqGroupXlsQrImg from '~/assets/img/qqgroupqr-xls.png';
+import QqGroupZxQrImg from '~/assets/img/qqgroupqr-zx.png';
 
 const props = defineProps<{
   modelValue: boolean;
 }>();
 const emit = defineEmits(['update:modelValue']);
 const active = ref(true);
+
+const items = [
+  {
+    url: 'https://qm.qq.com/q/lJye8TGb74',
+    img: QqGroupXlsQrImg,
+    btnText: '点击加入纳新QQ群(兴隆山校区)',
+    detailText: '群号：906743256'
+  },
+  {
+    url: 'https://qm.qq.com/q/QLLONiIvwA',
+    img: QqGroupZxQrImg,
+    btnText: '点击加入纳新QQ群(中心校区和其他)',
+    detailText: '群号：906965915'
+  }
+];
 </script>
 
 <template>
@@ -22,7 +38,6 @@ const active = ref(true);
           <div
             v-ripple
             class="cursor-pointer grid place-items-center w-6"
-            :style="{ backgroundColor: variants.mocha.crust.hex }"
             @click="active = !active"
           >
             <Icon
@@ -34,19 +49,22 @@ const active = ref(true);
               name="material-symbols:keyboard-double-arrow-right"
             ></Icon>
           </div>
-          <div class="flex flex-row items-center justify-center">
-            <div class="p-1">
-              <img class="h-14" :src="QqGroupQrImg" />
-            </div>
-            <div>
-              <VBtn
-                href="https://qm.qq.com/q/lJye8TGb74"
-                target="_blank"
-                variant="text"
-              >
-                点此加入纳新QQ群
-              </VBtn>
-              <p class="text-3.5 text-center">群号：906743256</p>
+          <div class="flex flex-col">
+            <div v-for="item in items" class="flex flex-row items-center">
+              <div class="p-1">
+                <img class="h-14" :src="item.img" />
+              </div>
+              <div>
+                <VBtn
+                  :href="item.url"
+                  target="_blank"
+                  variant="text"
+                  size="small"
+                >
+                  {{ item.btnText }}
+                </VBtn>
+                <p class="text-3.5 text-center">{{ item.detailText }}</p>
+              </div>
             </div>
           </div>
         </div>
